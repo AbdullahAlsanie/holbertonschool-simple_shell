f7e8852bb84b401463664af460a2006c96ae9846
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-07-03 20:41:01 +0300

    Fix: memory leaks, empty PATH, and consistent exec handling for simple_shell

--- shell_func.c
+++ shell_func.c
@@ -50,24 +50,6 @@ char **split_line(char *line)
 	return (tokens);
 }
 
-/**
- * _getenv - gets value of environment variable
- * @name: variable name
- * Return: pointer to value
- */
-char *_getenv(const char *name)
-{
-	int i = 0;
-	size_t len = strlen(name);
-
-	while (environ[i])
-	{
-		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-			return (environ[i] + len + 1);
-		i++;
-	}
-	return (NULL);
-}
 
 /**
  * free_args - frees tokens array
